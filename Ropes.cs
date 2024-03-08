06a4ab4dfc175d2656d8960632c24f9e815d2ac1
LukaNikolaisvili <jokerpoker1213@gmail.com>
2024-03-07 22:31:45 -0500

    fixed couple methods still issues in the Insert method

--- Ropes.cs
+++ Ropes.cs
@@ -27,14 +27,14 @@ public class Rope
     private class Node
     {
         public string Value;
-        public int size;
+        public int Size;
         public Node Left;
         public Node Right;
 
         public Node(string value)
         {
             Value = value;
-            size = 0;
+            Size = 0;
             Left = null;
             Right = null;
         }
@@ -43,64 +43,78 @@ public class Rope
     private Node root;
     Node node = new Node(null);
 
- 
+
     public Rope(string S)
     {
+        // String st = new("Hi");
         root = Build(S, 0, S.Length);
     }
 
-   // Insert string S at index i (5 marks).
+    // Insert string S at index i (5 marks).
     public void Insert(string S, int i)
     {
-    
+
+        Node rightSide = Split(this.root, i);
+
+        Node newNode = new Node(S);
+
+        Node leftSide = Concatenate(this.root, newNode);
+        
+        this.root = Concatenate(leftSide, rightSide);
+                                           //root.value
+        Console.WriteLine(newNode.Value); // it should be the root.value but the build method is not working for the Rope class!
+
+
     }
-   // Delete the substring S[i; j] (5 marks).
+    // Delete the substring S[i; j] (5 marks).
     public void Delete(int i, int j)
     {
-      
+
     }
 
     // Return the substring S[i; j] (6 marks).
     public string Substring(int i, int j)
     {
-        
+
         return "";
     }
     // Return the index of the rst occurrence of S; -1 otherwise (9 marks).
     public int Find(string S)
     {
-      
+
         return -1;
     }
 
     // Return the character at index i (3 marks).
     public char CharAt(int i)
     {
-  
+
         return ' ';
     }
-    
+
     // Return the index of the rst occurrence of character c (4 marks).
     public int IndexOf(char c)
     {
-       
+
         return -1;
     }
 
     // Reverse the string represented by the current rope (5 marks).
     public void Reverse()
     {
-     
+
     }
 
     // Return the length of the string (1 mark).
     public int Length()
     {
 
-        if (node.size != 0){
-            return node.size;
+        if (node.Size != 0)
+        {
+            return node.Size;
         }
-        else{
+        else
+        {
             return 0;
         }
     }
@@ -108,43 +122,152 @@ public class Rope
     // Return the string represented by the current rope (4 marks).
     public override string ToString()
     {
-       
+
         return "";
     }
 
     // Print the augmented binary tree of the current rope (4 marks).
     public void PrintRope()
     {
-     
+
     }
 
-   //private methods
+    //private methods
 
-   // Recursively build a balanced rope for S[i; j] and return its root (part of the constructor).
+    // Recursively build a balanced rope for S[i; j] and return its root (part of the constructor).
     private Node Build(string s, int i, int j)
     {
-       
+
         return null;
     }
 
     // Return the root of the rope constructed by concatenating two ropes with roots p and q (3 marks).
     private Node Concatenate(Node p, Node q)
     {
-     
-        return null;
+        Node root = new Node(null);
+
+        if (p == null || q == null)
+        {
+            return null;
+        }
+        else
+        {
+
+
+            root.Left = p;
+            root.Right = q;
+            root.Size = p.Size + q.Size;
+
+
+            if (p.Left == null && p.Right == null && q.Left == null && q.Right == null)
+            {
+                root.Value = p.Value + q.Value;
+
+            }
+
+        }
+
+
+        if (this.root != null)
+        {
+            Console.WriteLine(this.root.Value);
+        }
+        else
+        {
+            Console.WriteLine("Root is null.");
+        }
+
+        return root;
     }
 
     // Split the rope with root p at index i and return the root of the right subtree (9 marks).
     private Node Split(Node p, int i)
     {
-       
-        return null;
+        int goLeft;
+
+        if (p == null)
+        {
+            // try{
+            return null;
+
+            // }catch{
+            //     Exception(e)
+            // }
+
+
+        }
+
+        if (p.Left != null)
+        {
+            goLeft = p.Left.Size;
+        }
+
+        else
+        {
+            goLeft = 0;
+        }
+
+        if (i <= goLeft)
+        {
+
+
+            p.Left = Split(p.Left, i);
+
+            return p;
+
+        }
+        else
+        {
+            i -= goLeft; // + p.Value.Length;
+            Node rightSide = Split(p.Right, i);
+
+            p.Right = rightSide;
+
+            return rightSide;
+        }
+
+
+
     }
-    
+
+
     // Rebalance the rope using the algorithm found on pages 1319-1320 of Boehm et al. (9 marks).
     private Node Rebalance()
     {
-       
+
         return null;
     }
+
+
+    public static void Main(string[] args)
+    {
+        Node node = new Node("hello");
+        Node node1 = new Node("world");
+        Rope rope = new Rope("HelloWorld");
+
+        Console.WriteLine(rope.Split(node, 5));
+
+        Console.WriteLine(node.Value);
+
+        Node node3 = new Node("");
+
+        node3 = rope.Concatenate(node, node1);
+
+        Console.WriteLine(node3.Value);
+
+
+        rope.Insert("Farzad", 5);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
 }
