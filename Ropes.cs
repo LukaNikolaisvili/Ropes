32aabbb18970c19f9dd8b43dfd670b073f8a03d1
LukaNikolaisvili <jokerpoker1213@gmail.com>
2024-03-10 22:37:34 -0400

    I have added changes that freddy did for the Ropes fixed the insert***

--- Ropes.cs
+++ Ropes.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿﻿/*
 COIS-3020 ASSIGNMENT 2
 CONTRIBUTORS:
 LUKA NIKOLAISVILI
@@ -61,6 +61,7 @@ public class Rope
     }
 
     // Insert string S at index i (5 marks).
+   // Insert string S at index i (5 marks).
     public void Insert(string S, int i)
     {
         try
@@ -68,18 +69,54 @@ public class Rope
 
             if (S != "" || i == -1)
             {
-                // Split the node at index i
-                Node rightSide = Split(this.root, i);
+                if (i == 1)
+                {
+                    // string L = Substring (0,i);
+                    // Node leftside = new Node(L);
 
-                // Create a new node to hole the string to be inserted
-                Node newNode = new Node(S);
+                    // Split the node at index i
+                    Node rightSide = (this.root);
 
-                // concatenate the current node with the new node
-                Node leftSide = Concatenate(this.root, newNode);
+                    // Create a new node to hole the string to be inserted
+                    Node newNode = new Node(S);
 
-                Console.WriteLine("The size of S is in IF:" + S.Length);
-                // concatenate the left side of the tree with the tree exported from split
-                this.root = Concatenate(leftSide, rightSide);
+
+
+                    // concatenate the current node with the new node
+                    // Node leftSide = Concatenate(this.root, newNode);
+
+                    Console.WriteLine("The size of S is in IF:" + S.Length);
+
+
+                    // concatenate the left side of the tree with the tree exported from split
+                    this.root = Concatenate(newNode, rightSide);
+
+
+                }
+                else
+                {
+                    string L = Substring(0, i-1);
+                    Node leftside = new Node(L);
+
+                    // Split the node at index i
+                    Node rightSide = Split(this.root, i);
+
+                    // Create a new node to hole the string to be inserted
+                    Node newNode = new Node(S);
+
+
+                    // concatenate the current node with the new node
+                    // Node leftSide = Concatenate(this.root, newNode);
+
+                    Console.WriteLine("The size of S is in IF:" + S.Length);
+
+
+                    // concatenate the left side of the tree with the tree exported from split
+                    Node v1 = Concatenate(newNode, rightSide);
+                    this.root = Concatenate(leftside, v1);
+
+                }
+
 
 
             }
@@ -389,56 +426,18 @@ public class Rope
     // Return the string represented by the current rope (4 marks).
     public override string ToString()
     {
-        try
-        {
-            if (root == null)
-            {
-                // No node so return empty string
-                return "";
-            }
-            else
-            {
-                // Use stringbuilder and stack for recursion
-                StringBuilder strBuild = new StringBuilder();
-                Stack<Node> stack = new Stack<Node>();
-                stack.Push(root);
-
-                while (stack.Count > 0)
-                {
-                    Node curr = stack.Pop();
-
-                    if (curr.Value != null)
-                    {
-                        // If value in current node add to the stringbuilder
-                        strBuild.Append(curr.Value);
-                    }
-                    else
-                    {
-                        if (curr.Right != null)
-                        {
-                            // Add right child onto stack
-                            stack.Push(curr.Right);
-                        }
-
-                        if (curr.Left != null)
-                        {
-                            // Add left child onto stack
-                            stack.Push(curr.Left);
-                        }
-                    }
-                }
-                // Return the built string
-                return strBuild.ToString();
-            }
-        }
-        catch (Exception ex)
-        {
-            // Handle the exception here, you can log it or perform any other necessary actions.
-            Console.WriteLine("An error occurred: " + ex.Message);
-            return ""; // Return an empty string or any default value to indicate an error.
-        }
+        return ToString(root);
     }
 
+    private string ToString(Node node)
+    {
+        if (node == null) return "";
+        if (node.Value != null)
+        {
+            return node.Value;
+        }
+        return ToString(node.Left) + ToString(node.Right);
+    }
 
     // Print the augmented binary tree of the current rope (4 marks).
     public void PrintRope()
@@ -483,39 +482,37 @@ public class Rope
     //private methods
 
     // Recursively build a balanced rope for S[i; j] and return its root (part of the constructor).
-    private Node Build(string s, int i, int j)
+    private Node Build(string str, int start, int end)
     {
-        // If the substring is zero or -, return null.
-        if (i >= j)
+        if (start >= end) return null;
+        if (end - start <= 10) return new Node(str.Substring(start, end - start));
+
+        int mid = start + (end - start) / 2;
+        Node left = Build(str, start, mid);
+        Node right = Build(str, mid, end);
+
+        Node parent = new Node(null)
         {
-            return null;
-        }
 
-
-        // create and return a leaf node If the substring length is small enough,.
-        if (j - i == 1)
-        {
-            return new Node(s.Substring(i, j - i)) { Size = j - i };
-        }
-
-        // calculating the mid point to use it and to split in half (so it will split it in half)
-        int mid = i + (j - i) / 2;
-
-        // using the recursion to build the left and right sides of the tree (using the runtime stack)
-        Node left = Build(s, i, mid);
-        Node right = Build(s, mid, j);
-
-        // Create a new internal node (non-leaf) as the parent of the two subtrees.
-        Node internalNode = new Node(null)
-        {
             Left = left,
-            Right = right,
-            // The weight of an internal node is the total length of strings in its left subtree.
-            // This assumes that all string content is stored in leaf nodes.
-            Size = (left != null ? left.Size : 0) + (right != null ? right.Size : 0)
-        };
+            Right = right
 
-        return internalNode;
+        };
+        int leftSize = 0;
+        int rightSize = 0;
+
+        if (left != null)
+        {
+            leftSize = left.Size;
+        }
+
+        if (right != null)
+        {
+            rightSize = right.Size;
+        }
+
+        parent.Size = leftSize + rightSize;
+        return parent;
     }
 
     // Return the root of the rope constructed by concatenating two ropes with roots p and q (3 marks).
@@ -636,15 +633,15 @@ public class Rope
     public static void Main(string[] args)
     {
         // Testing nodes
-        Node node = new Node("LUKA");
+        // Node node = new Node("LUKA");
         Node node1 = new Node("NIKOLAISVILI");
-        Rope rope = new Rope(node.Value + node1.Value);
+        Rope rope = new Rope(node1.Value);
 
-        Console.WriteLine(rope.Split(node, 1));
+        // Console.WriteLine(rope.Split(node, 1));
 
         // Console.WriteLine(node.Value);
 
-        Node node3 = new Node(""); // L U K A N I K O
+        Node node3 = new Node(null); // L U K A N I K O
 
         // node3 = rope.Concatenate(node, node1);
 
@@ -680,7 +677,7 @@ public class Rope
         // Console.WriteLine(rope.ToString());
 
 
-        Console.WriteLine(rope.IndexOf(node, 'L'));
+        // Console.WriteLine(rope.IndexOf(node, 'L'));
 
         // Console.WriteLine(rope.Split(node, 1));
 
@@ -739,7 +736,7 @@ public class Rope
 
                 if (convertStartIndexToInt == true && index != -1)
                 {
-                    Console.WriteLine(rope.Split(node, index));
+                    Console.WriteLine(rope.Split(node1, index));
                     Console.WriteLine("\nSplitted succesfully at index " + index);
                 }
 
